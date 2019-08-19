f5a6d075806b37eda8d543feb44d80337facbcd9
Ameer Jhan <ameerjhanprof@gmail.com>
2019-08-19 20:17:36 +0530

    feat: add more details to the pod

--- components/pods.js
+++ components/pods.js
@@ -1,9 +1,96 @@
 const React = require('react');
-const { Text } = require('ink');
 const propTypes = require('prop-types');
+const { Color } = require('ink');
+const { Box } = require('ink');
 
-const PodsComponent = (props) => {  
-  return props.pods.map(pod => <Text key={pod.metadata.name}>{pod.metadata.name}</Text>);
+const findMaxLengthText = (array, header) => {
+  let maxLength = header.length;
+
+  array.forEach(element => {
+    if(element[header].text.length > maxLength) {
+      maxLength = element[header].text.length;
+    }
+  });
+
+  return maxLength;
+}
+
+const emptySpaces = (number) => {
+  return new Array(number).join(' ');
+}
+
+const colorizeText = (content, maxLength) => {
+  if(content.color == undefined && content.bgColor == undefined) {
+    return content.text;
+  }
+  else {
+    const props = {};
+
+    if(content.color != undefined) {
+      props[content.color] = true;
+    } 
+    if(content.bgColor != undefined) {
+      props['bgKeyword'] = content.bgColor; 
+    }
+
+    const extraSpaceArountText = 2;
+    const spaceAroundText = Math.ceil((maxLength - content.text.length) / 2) + extraSpaceArountText;
+    const prettyText = `${emptySpaces(spaceAroundText)}${content.text}${emptySpaces(spaceAroundText)}`;
+
+    return <Color {...props}>{prettyText}</Color>
+  }
+}
+
+const PodsComponent = ({ pods }) => {
+  if(pods.length > 0) {
+    const tableHeaderTexts = Object.keys(pods[0]);
+    const cellSpacing = 5;
+    const maxLengthOfTextInHeader = {};
+
+    tableHeaderTexts.forEach(header => {
+      maxLengthOfTextInHeader[header] = findMaxLengthText(pods, header);
+    });
+    
+    const tableContent = pods.map((pod, row) => {
+      const tableContent = [];
+
+      tableHeaderTexts.forEach((header, column) => {
+        tableContent.push(
+          <Box key={column} width={maxLengthOfTextInHeader[header] + cellSpacing}>
+            {colorizeText(pod[header], maxLengthOfTextInHeader[header])}
+          </Box>
+        );
+      });
+
+      return (
+        <Box key={row}>
+          {tableContent}
+        </Box>
+      );
+    });
+
+    const tableHeader = [];
+
+    tableHeaderTexts.forEach((header, index) => {
+      tableHeader.push(
+        <Box key={header} width={maxLengthOfTextInHeader[header] + cellSpacing}>
+          <Color green>{header.toUpperCase()}</Color>
+        </Box>
+      )
+    });
+
+    return <React.Fragment>
+      <Box paddingLeft={1} marginTop={1} flexDirection="row">
+        {tableHeader}
+      </Box>
+      <Box paddingLeft={1} flexDirection="column">
+        {tableContent}
+      </Box>
+    </React.Fragment>
+  }
+  else {
+    return "";
+  }
 }
 
 PodsComponent.propTypes = {
--- containers/pods.js
+++ containers/pods.js
@@ -4,19 +4,104 @@ const importJsx = require('import-jsx');
 const PodsComponent = importJsx('../components/pods');
 const { Component } = require('react');
 const propTypes = require('prop-types');
+const TimeAgo = require('javascript-time-ago');
+const en = require('javascript-time-ago/locale/en');
 
 class Pods extends Component {
   constructor(props) {
     super(props);
     this.state = { pods: [] };
+    this.timer;
+    TimeAgo.addLocale(en);
+    this.timeAgo = new TimeAgo('en-US');
   }
 
   componentWillUpdate(nextProps) {
     if(this.props.namespace != nextProps.namespace) {
-      k8sApi.listNamespacedPod(nextProps.namespace).then(response => {
-        this.setState({ pods: response.body.items });
+      this.listenForChanges(nextProps.namespace);
+    }
+  }
+
+  componentWillUnmount() {
+    if(this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  listenForChanges(namespace) {
+    if(this.timer) {
+      clearInterval(this.timer);
+    }
+    
+    this.timer = setInterval(() => {
+      k8sApi.listNamespacedPod(namespace).then(response => {
+        this.setState({ pods: this.transformPodData(response.body.items) });
+      });
+    }, 500);
+  }
+
+  colorCodeStatus(status) {
+    switch(status) {
+      case "Terminating":
+        return { bgColor: "red", color: "white" }
+      case "OutOfmemory":
+        return { bgColor: "red", color: "white" }
+      case "Failed":
+        return { bgColor: "red", color: "white" }
+      case "Pending":
+        return { bgColor: "yellow", color: "white" }
+      case "Succeeded":
+        return { bgColor: "green", color: "white" }
+      case "Running":
+        return { bgColor: "green", color: "white" }
+      default:
+        return status;     
+    }
+  }
+
+  transformPodData(items) {
+    let pods = [];
+
+    for(let i = 0; i < items.length; i++) {
+      let podStatus;
+      const item = items[i];
+
+      const itemStatus = item.status;
+
+      podStatus = itemStatus.phase;
+
+      if(itemStatus.reason) {
+        podStatus = itemStatus.reason;
+      }
+
+      if(item.metadata.deletionTimestamp) {
+        podStatus = 'Terminating'
+      }
+
+      let readyContainers = 0;
+      let restartCount = 0;
+
+      if(itemStatus.containerStatuses) {
+        itemStatus.containerStatuses.forEach(containerStatus => {
+          if(containerStatus.ready) {
+            readyContainers++;
+          }
+          restartCount += containerStatus.restartCount;
+        });
+      }
+
+      pods.push({
+        name: { text: item.metadata.name },
+        ready: { text: `${readyContainers}/${item.spec.containers.length}` },
+        status: { text: podStatus, ...this.colorCodeStatus(podStatus) },
+        restarts: { text: restartCount },
+        age: {
+          text: this.timeAgo.format(item.status.startTime, { flavour: 'tiny' })
+        }
       });
     }
+    
+    return pods;
   }
 
   render() {
