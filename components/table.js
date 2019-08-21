9942bce36462c9b72aec56552134bbd174e8f01f
Ameer Jhan <ameerjhanprof@gmail.com>
2019-08-21 18:59:05 +0530

    feat: add selection to table component

--- components/table.js
+++ components/table.js
@@ -3,133 +3,185 @@ const { Color } = require('ink');
 const { Box } = require('ink');
 const PropTypes = require('prop-types');
 
-/**
- * For an array of [{ name: 'ameer', city: 'madurai' }, { name: 'bala', city: 'bengaluru' }] it returns { name: 5, city: 9 }
- * @param {*} array array of objects
- * @param {*} header key of the object for which the max length its value in the entire array is to be found
- * @returns key value pair of header and its length of its max length string in the entire array
- */
-const findMaxLengthText = (array, header) => {
-  let maxLength = header.length;
-
-  array.forEach(element => {
-    const text = new String(element[header].text);
-    if(text.trim().length > maxLength) {
-      maxLength = text.trim().length;
-    }
-  });
-
-  return maxLength;
-}
-
-/**
- * Creates string with specified number of spaces
- * @param {number} noOfSpaces number spaces to be added to the empty string
- * @returns string with specified number of spaces
- */
-const emptySpaces = (noOfSpaces) => {
-  return new Array(noOfSpaces).join(' ');
-}
-
-/**
- * if the string is **'Ameer'** and the noOfSpaces is **4** then it would return '\*\*Ameer\*\*' where each * represents a blank space
- * @param {string} string string content which is to be padded
- * @param {number} noOfSpaces number of spaces that is to be padded around the text
- */
-const padAroundStringWithSpaces = (string, noOfSpaces, extraSpace = 0) => {
-  const text = new String(string);
-  const spaceAtOneEnd = Math.ceil((noOfSpaces - text.length) / 2) + extraSpace;
-  const paddedText = `${emptySpaces(spaceAtOneEnd)}${text}${emptySpaces(spaceAtOneEnd)}`;
-
-  return paddedText;
-}
-
-/**
- * It wraps the text content with specified font and background colors
- * @param {string} content string content that is to be displayed
- * @returns string content wrapped in Color component with specified font and background colors
- */
-const colorizeText = (content) => {
-  if(content.color == undefined && content.bgColor == undefined) {
-    return content.text;
+const SelectionHighlighterComponent = ({ content, isSelected }) => {
+  if(isSelected) {
+    return <Color bgBlue white>
+            {content}
+          </Color>;
   }
   else {
-    const props = {};
-
-    if(content.color != undefined) {
-      props[content.color] = true;
-    } 
-    if(content.bgColor != undefined) {
-      props['bgKeyword'] = content.bgColor; 
-    }
-
-    return <Color {...props}>{content.text}</Color>
+    return <React.Fragment>{content}</React.Fragment>;
   }
 }
 
-const TableComponent = ({ data, cellSpacing }) => {
-  if(!data || data.length == 0) {
-    return "";
+SelectionHighlighterComponent.propTypes = {
+  content: PropTypes.any.isRequired,
+  isSelected: PropTypes.bool.isRequired
+};
+class TableComponent extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = { selectedIndex: 0 }
   }
 
-  const tableHeaderTexts = Object.keys(data[0]);
-  const maxLengthOfTextInHeader = {};
-
-  // Default cell spacing of 5
-  if(!cellSpacing) {
-    cellSpacing = 5;
-  }
-
-  tableHeaderTexts.forEach(header => {
-    maxLengthOfTextInHeader[header] = findMaxLengthText(data, header);
-  });
-
-  // Pad texts with spaces if needed
-  data.forEach(row => {
-    tableHeaderTexts.forEach(header => {
-      // Pad text specifies extra space that is to the cell 
-      if(row[header].padText) {
-        row[header].text = padAroundStringWithSpaces(row[header].text, maxLengthOfTextInHeader[header], row[header].extraPadding);
+  componentDidMount() {
+    process.stdin.on('keypress', (chunk, key) => {
+      if(this.props.data.length > 0) {
+        if(key.name == 'down') {
+          this.setState({ selectedIndex: (this.state.selectedIndex + 1) % this.props.data.length });
+        }
+        else if(key.name == 'up') {
+          if(this.state.selectedIndex == 0) {
+            this.setState({ selectedIndex: this.props.data.length - 1 });
+          }
+          else {
+            this.setState({ selectedIndex: this.state.selectedIndex - 1 });
+          }
+        }
       }
     });
-  });
+  }
 
-  const tableContent = data.map((row, rowIndex) => {
-    const tableContent = [];
+  // TODO: refactor this dirty workaround
+  componentWillUpdate(nextProps) {
+    if(this.props.data.length != nextProps.data.length) {
+      this.state.selectedIndex = 0;
+    }
+  }
 
-    tableHeaderTexts.forEach((header, columnIndex) => {
-      tableContent.push(
-        <Box key={columnIndex} width={maxLengthOfTextInHeader[header] + cellSpacing}>
-          {colorizeText(row[header], maxLengthOfTextInHeader[header])}
+  /**
+   * For an array of [{ name: 'ameer', city: 'madurai' }, { name: 'bala', city: 'bengaluru' }] it returns { name: 5, city: 9 }
+   * @param {*} array array of objects
+   * @param {*} header key of the object for which the max length its value in the entire array is to be found
+   * @returns key value pair of header and its length of its max length string in the entire array
+   */
+  findMaxLengthText (array, header) {
+    let maxLength = header.length;
+
+    array.forEach(element => {
+      const text = new String(element[header].text);
+      if(text.trim().length > maxLength) {
+        maxLength = text.trim().length;
+      }
+    });
+
+    return maxLength;
+  }
+
+  /**
+   * Creates string with specified number of spaces
+   * @param {number} noOfSpaces number spaces to be added to the empty string
+   * @returns string with specified number of spaces
+   */
+  emptySpaces(noOfSpaces) {
+    return new Array(noOfSpaces).join(' ');
+  }
+
+  /**
+   * if the string is **'Ameer'** and the noOfSpaces is **4** then it would return '\*\*Ameer\*\*' where each * represents a blank space
+   * @param {string} string string content which is to be padded
+   * @param {number} noOfSpaces number of spaces that is to be padded around the text
+   */
+  padAroundStringWithSpaces(string, noOfSpaces, extraSpace = 0) {
+    const text = new String(string);
+    const spaceAtOneEnd = Math.ceil((noOfSpaces - text.length) / 2) + extraSpace;
+    const paddedText = `${this.emptySpaces(spaceAtOneEnd)}${text}${this.emptySpaces(spaceAtOneEnd)}`;
+
+    return paddedText;
+  }
+
+  /**
+   * It wraps the text content with specified font and background colors
+   * @param {string} content string content that is to be displayed
+   * @returns string content wrapped in Color component with specified font and background colors
+   */
+  colorizeText(content) {
+    if(content.color == undefined && content.bgColor == undefined) {
+      return content.text;
+    }
+    else {
+      const props = {};
+
+      if(content.color != undefined) {
+        props[content.color] = true;
+      } 
+      if(content.bgColor != undefined) {
+        props['bgKeyword'] = content.bgColor; 
+      }
+
+      return <Color {...props}>{content.text}</Color>
+    }
+  }
+
+  render() {
+    const data = this.props.data;
+    let cellSpacing = this.props.cellSpacing;
+
+    if(!data || data.length == 0) {
+      return "";
+    }
+    const tableHeaderTexts = Object.keys(data[0]);
+    const maxLengthOfTextInHeader = {};
+  
+    // Default cell spacing of 5
+    if(!cellSpacing) {
+      cellSpacing = 5;
+    }
+  
+    tableHeaderTexts.forEach(header => {
+      maxLengthOfTextInHeader[header] = this.findMaxLengthText(data, header);
+    });
+  
+    // Pad texts with spaces if needed
+    data.forEach(row => {
+      tableHeaderTexts.forEach(header => {
+        // Pad text specifies extra space that is to the cell 
+        if(row[header].padText) {
+          row[header].text = this.padAroundStringWithSpaces(row[header].text, maxLengthOfTextInHeader[header], row[header].extraPadding);
+        }
+      });
+    });
+  
+    const tableContent = data.map((row, rowIndex) => {
+      const tableContent = [];
+  
+      tableHeaderTexts.forEach((header, columnIndex) => {
+        const isSelected = (row[header].isSelector && rowIndex == this.state.selectedIndex)? true: false;
+        const tableContentTextRef = this.colorizeText(row[header], maxLengthOfTextInHeader[header]);
+        
+        tableContent.push(
+          <Box key={columnIndex} width={maxLengthOfTextInHeader[header] + cellSpacing}>
+            <SelectionHighlighterComponent content={tableContentTextRef} isSelected={isSelected} />
+          </Box>
+        );
+      });
+  
+      return (
+        <Box key={rowIndex}>
+          {tableContent}
         </Box>
       );
     });
-
-    return (
-      <Box key={rowIndex}>
+  
+    const tableHeader = [];
+  
+    tableHeaderTexts.forEach((header, index) => {
+      tableHeader.push(
+        <Box key={index} width={maxLengthOfTextInHeader[header] + cellSpacing}>
+          <Color green>{header.toUpperCase()}</Color>
+        </Box>
+      )
+    });
+  
+    return <React.Fragment>
+      <Box flexDirection="row">
+        {tableHeader}
+      </Box>
+      <Box flexDirection="column">
         {tableContent}
       </Box>
-    );
-  });
-
-  const tableHeader = [];
-
-  tableHeaderTexts.forEach((header, index) => {
-    tableHeader.push(
-      <Box key={index} width={maxLengthOfTextInHeader[header] + cellSpacing}>
-        <Color green>{header.toUpperCase()}</Color>
-      </Box>
-    )
-  });
-
-  return <React.Fragment>
-    <Box flexDirection="row">
-      {tableHeader}
-    </Box>
-    <Box flexDirection="column">
-      {tableContent}
-    </Box>
-  </React.Fragment>
+    </React.Fragment>
+  }
 };
 
 TableComponent.propTypes = {
