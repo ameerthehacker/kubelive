2120c5b1b789ab729de7dab85ca72061f086039a
Ameer Jhan <ameerjhanprof@gmail.com>
2019-08-25 19:49:45 +0530

    test(pods): add rendering tests for pods component

---
+++ __test__/components/pods.test.js
@@ -0,0 +1,59 @@
+const React = require('react');
+const { shallow } = require('enzyme');
+const executeActionMock = jest.fn();
+const mockActions = {
+  actions: [
+    {
+      key: 'c',
+      descrition: 'Copy'
+    }
+  ],
+  executeAction: executeActionMock
+};
+jest.mock('../../actions/pod', () => mockActions);
+const { actions } = require('../../actions/pod');
+const importJsx = require('import-jsx');
+const PodsComponent = importJsx('../../components/pods');
+const TableComponent = importJsx('../../components/table');
+
+describe('PodsComponent', () => {
+  let component;
+  let tableComponent;
+  const pods = [
+    {
+      name: 'some-pod-name'
+    }
+  ];
+  let namespace = 'some-name';
+
+  beforeEach(() => {
+    component = shallow(<PodsComponent pods={pods} namespace={namespace} />);
+    tableComponent = component.find(TableComponent).first();
+    executeActionMock.mockClear();
+  });
+
+  it('should match the snapshot', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('tableComponent should get passed the data props as pods', () => {
+    expect(tableComponent.props().data).toEqual(pods);
+  });
+
+  it('tableComponent should get passed the namespace props', () => {
+    expect(tableComponent.props().namespace).toEqual(namespace);
+  });
+
+  it('tableComponent should get passed the actions props', () => {
+    expect(tableComponent.props().actions).toEqual(actions);
+  });
+
+  it('tableComponent should call executeAction(key, name, namespace) when onActionPerformed is called', () => {
+    const key = { name: 'c' };
+    const name = 'some-pod-name';
+
+    tableComponent.invoke('onActionPerformed')({ key, name, namespace });
+
+    expect(executeActionMock).toHaveBeenCalledWith(key, name, namespace);
+  });
+});
