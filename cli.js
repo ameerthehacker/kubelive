e78a232c08ed9e2cfea6c7ff782b1fe5de3a683a
Ameer Jhan <ameerjhanprof@gmail.com>
2019-08-26 19:59:21 +0530

    feat: add basic cli

--- cli.js
+++ cli.js
@@ -3,5 +3,16 @@ const React = require('react');
 const importJsx = require('import-jsx');
 const App = importJsx('./containers/app');
 const { render } = require('ink');
+const cli = require('commander');
+const version = require('./package.json').version;
 
-render(<App />);
+cli.version(version, '-v, --version');
+
+cli
+  .command('get <resource>')
+  .description('get live update on the <resource>')
+  .action((resource) => {
+    render(<App resource={resource} />);
+  });
+
+cli.parse(process.argv);
--- containers/app.js
+++ containers/app.js
@@ -4,6 +4,8 @@ const { Component } = require('react');
 const importJsx = require('import-jsx');
 const Namespaces = importJsx('./namespaces');
 const Pods = importJsx('./pods');
+const PropTypes = require('prop-types');
+const { Color } = require('ink');
 
 class App extends Component {
   constructor(props) {
@@ -16,14 +18,34 @@ class App extends Component {
     this.setState({ selectedNamespace: name });
   }
 
+  getResourceComponent() {
+    switch (this.props.resource) {
+      case 'pod':
+      case 'pods':
+        return <Pods namespace={this.state.selectedNamespace} />;
+      default:
+        return false;
+    }
+  }
+
   render() {
-    return (
-      <React.Fragment>
-        <Namespaces onNamespaceChange={this.onNamespaceChange} />
-        <Pods namespace={this.state.selectedNamespace} />
-      </React.Fragment>
-    );
+    const resourceComponent = this.getResourceComponent();
+
+    if (resourceComponent) {
+      return (
+        <React.Fragment>
+          <Namespaces onNamespaceChange={this.onNamespaceChange} />
+          {resourceComponent}
+        </React.Fragment>
+      );
+    } else {
+      return <Color red>resource {this.props.resource} is not supported</Color>;
+    }
   }
 }
+
+App.propTypes = {
+  resource: PropTypes.string.isRequired
+};
 
 module.exports = App;
