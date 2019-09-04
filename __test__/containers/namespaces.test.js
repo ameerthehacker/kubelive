41e11d27dbda28dbee7b58c20e0f925c2c6af6c5
Ameer Jhan <ameerjhanprof@gmail.com>
2019-09-04 16:34:00 +0530

    ref: refactor tests of namespaces container to make it simple

--- __test__/containers/namespaces.test.js
+++ __test__/containers/namespaces.test.js
@@ -7,114 +7,129 @@ const k8sApi = require('../../kube/api');
 const { shallow } = require('enzyme');
 const { Color } = require('ink');
 
-const createNamespaceContainer = (props, listNamespaceMock) => {
-  k8sApi.listNamespace = listNamespaceMock;
+describe('Namespaces', () => {
+  const createNamespaceContainer = (props, listNamespaceMock) => {
+    k8sApi.listNamespace = listNamespaceMock;
+    const namespacesContainer = new Namespaces(props);
+    namespacesContainer.setState = jest.fn();
 
-  return shallow(<Namespaces {...props} />);
-};
+    return namespacesContainer;
+  };
+
+  describe('componentDidMount()', () => {
+    it('should call k8Api.listNamespace()', () => {
+      const listNamespaceMock = jest.fn().mockResolvedValue({
+        body: {
+          items: []
+        }
+      });
+      const namespaces = createNamespaceContainer({}, listNamespaceMock);
+
+      namespaces.componentDidMount();
+
+      expect(k8sApi.listNamespace).toHaveBeenCalled();
+    });
+
+    it('should call this.props.onNamespaceChange(namespace) when atleast one namespace available', () => {
+      expect.assertions(1);
+      const listNamespaceMock = jest.fn().mockResolvedValue({
+        body: {
+          items: [
+            {
+              metadata: { name: 'some-namespace' }
+            }
+          ]
+        }
+      });
+      const onNamespaceChangeMock = jest.fn();
+      const namespaces = createNamespaceContainer(
+        {
+          onNamespaceChange: onNamespaceChangeMock
+        },
+        listNamespaceMock
+      );
+
+      namespaces.componentDidMount();
+
+      listNamespaceMock().then((response) => {
+        expect(onNamespaceChangeMock).toHaveBeenCalledWith(
+          response.body.items[0].metadata.name
+        );
+      });
+    });
+  });
+});
 
 describe('Namespaces', () => {
-  it('should match the snapshot', () => {
-    const listNamespaceMock = jest.fn().mockResolvedValue({
-      body: {
-        items: []
-      }
+  const createNamespaceContainer = (props, listNamespaceMock) => {
+    k8sApi.listNamespace = listNamespaceMock;
+
+    return shallow(<Namespaces {...props} />);
+  };
+
+  describe('render()', () => {
+    it('should match the snapshot', () => {
+      const listNamespaceMock = jest.fn().mockResolvedValue({
+        body: {
+          items: []
+        }
+      });
+      const namespaces = createNamespaceContainer({}, listNamespaceMock);
+
+      expect(namespaces).toMatchSnapshot();
     });
-    const namespaces = createNamespaceContainer({}, listNamespaceMock);
 
-    expect(namespaces).toMatchSnapshot();
-  });
+    it('should set the namespaces props to resolved props', () => {
+      const items = [
+        {
+          metadata: { name: 'namespace1' }
+        }
+      ];
+      const listNamespaceMock = jest.fn().mockResolvedValue({
+        body: {
+          items
+        }
+      });
+      const namespaces = createNamespaceContainer({}, listNamespaceMock);
 
-  it('should call k8Api.listNamespace() when component mounts', () => {
-    const listNamespaceMock = jest.fn().mockResolvedValue({
-      body: {
-        items: []
-      }
-    });
-    createNamespaceContainer({}, listNamespaceMock);
+      namespaces.setState({ namespaces: items });
 
-    expect(k8sApi.listNamespace).toHaveBeenCalled();
-  });
-
-  it('should call this.props.onNamespaceChange(namespace) when component mounts with atleast one namespace available', () => {
-    expect.assertions(1);
-    const listNamespaceMock = jest.fn().mockResolvedValue({
-      body: {
-        items: [
-          {
-            metadata: { name: 'some-namespace' }
-          }
-        ]
-      }
-    });
-    const onNamespaceChangeMock = jest.fn();
-    createNamespaceContainer(
-      {
-        onNamespaceChange: onNamespaceChangeMock
-      },
-      listNamespaceMock
-    );
-
-    listNamespaceMock().then((response) => {
-      expect(onNamespaceChangeMock).toHaveBeenCalledWith(
-        response.body.items[0].metadata.name
-      );
-    });
-  });
-
-  it('should set the namespaces props to resolved props', () => {
-    expect.assertions(1);
-    const listNamespaceMock = jest.fn().mockResolvedValue({
-      body: {
-        items: [
-          { metadata: { name: 'namespaces1' } },
-          { metadata: { name: 'namespaces2' } }
-        ]
-      }
-    });
-    const namespaces = createNamespaceContainer({}, listNamespaceMock);
-
-    listNamespaceMock().then((response) => {
       namespaces.update();
       const namespacesComponent = namespaces.find(NamespacesComponent).first();
-      expect(namespacesComponent.props().namespaces).toEqual(
-        response.body.items
+      expect(namespacesComponent.props().namespaces).toEqual(items);
+    });
+
+    it('should set the onNamespaceChange props to this.props.onNamespaceChange', () => {
+      const listNamespaceMock = jest.fn().mockResolvedValue({
+        body: {
+          items: []
+        }
+      });
+      const onNamespaceChange = () => {};
+
+      const namespaces = createNamespaceContainer(
+        { onNamespaceChange },
+        listNamespaceMock
+      );
+
+      const namespacesComponent = namespaces.find(NamespacesComponent).first();
+      expect(namespacesComponent.props().onNamespaceChange).toBe(
+        onNamespaceChange
       );
     });
-  });
 
-  it('should set the onNamespaceChange props to this.props.onNamespaceChange', () => {
-    const listNamespaceMock = jest.fn().mockResolvedValue({
-      body: {
-        items: []
-      }
-    });
-    const onNamespaceChange = () => {};
-
-    const namespaces = createNamespaceContainer(
-      { onNamespaceChange },
-      listNamespaceMock
-    );
-
-    const namespacesComponent = namespaces.find(NamespacesComponent).first();
-    expect(namespacesComponent.props().onNamespaceChange).toBe(
-      onNamespaceChange
-    );
-  });
-
-  it('should return error with red color when the promise fails', () => {
-    expect.assertions(1);
-    const listNamespaceMock = jest.fn().mockRejectedValue({
-      code: 'ENOTFOUND'
-    });
-    const namespaces = createNamespaceContainer({}, listNamespaceMock);
-
-    listNamespaceMock()
-      .then()
-      .catch((err) => {
-        namespaces.update();
-        const colorComponent = namespaces.find(Color).first();
-        expect(colorComponent.render().text()).toContain(err.code);
+    it('should return error with red color when the promise fails', () => {
+      const errCode = 'ENOTFOUND';
+      const listNamespaceMock = jest.fn().mockRejectedValue({
+        code: errCode
       });
+      const namespaces = createNamespaceContainer({}, listNamespaceMock);
+
+      namespaces.setState({ err: errCode });
+
+      namespaces.update();
+      const colorComponent = namespaces.find(Color).first();
+      expect(colorComponent.render().text()).toContain(errCode);
+    });
   });
 });
